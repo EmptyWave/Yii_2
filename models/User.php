c88a1030c347522b100cc065fa807ca9be586d76
EmptyWave <dimonyud@gmail.com>
2019-04-21 02:30:12 +0300

    add lesson2 hw, with db

--- models/User.php
+++ models/User.php
@@ -2,103 +2,99 @@
 
 namespace app\models;
 
+use app\models\tables\Users;
+
 class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
+  public $id;
+  public $username;
+  public $passwordHash;
+  public $authKey;
+  public $accessToken;
+  public $email;
+  public $phone;
+  public $userType_id;
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
+  /**
+   * {@inheritdoc}
+   */
+  public static function findIdentity($id)
+  {
+    $user = Users::findOne($id)->attributes;
+    if (!is_null($user))
+      return new static($user);
 
+    return null;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentity($id)
-    {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public static function findIdentityByAccessToken($token, $type = null)
+  {
+    $user = Users::findOne(['accessToken' => $token])->attributes;
+    if (!is_null($user))
+      return new static($user);
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function findIdentityByAccessToken($token, $type = null)
-    {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
+    return null;
+  }
 
-        return null;
-    }
+  /**
+   * Finds user by username
+   *
+   * @param string $username
+   * @return static|null
+   */
+  public static function findByUsername($username)
+  {
+    $user = Users::findOne(['username' => $username])->attributes;
+    if (!is_null($user))
+      return new static($user);
 
-    /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
+    return null;
+  }
 
-        return null;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getAuthKey()
+  {
+    return $this->authKey;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
+  public function getHash()
+  {
+    return $this->passwordHash;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
+  public function setPasswordHash($password){
+    $this->passwordHash = \Yii::$app->getSecurity()->generatePasswordHash($password);
+  }
 
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function validateAuthKey($authKey)
+  {
+    return $this->authKey === $authKey;
+  }
+
+  /**
+   * Validates password
+   *
+   * @param string $password password to validate
+   * @return bool if password provided is valid for current user
+   */
+
+  public function validatePassword($password){
+    return \Yii::$app->getSecurity()->validatePassword($password, $this->passwordHash);
+  }
 }
--- views/site/index.php
+++ views/site/index.php
@@ -7,3 +7,8 @@ $this->title = 'Task Manager+';
 
 <h3>Тут могли быть ваши таски (ну т.е. все таски, мб даже история)</h3>
 <h5>homeURL - <?=Yii::$app->homeUrl?></h5>
+<h5>admin_hash - <?=Yii::$app->getSecurity()->generatePasswordHash('admin')?></h5>
+<!--<h5>admin_verify - <?/*=Yii::$app->getSecurity()->validatePassword('admin', '$2y$13$E5u6.FFZ2XmGhk1GpyarIe.5bzavRH18mASAtGWdZ/rewriqYmtlK')*/?></h5>
+<h5>demo_hash - <?/*=Yii::$app->getSecurity()->generatePasswordHash('demo')*/?></h5>
+<h5>demo_verify - <?/*=Yii::$app->getSecurity()->validatePassword('demo', '$2y$13$teOAILNJrfroJ9zqNKBiKOyVGiY7IH4TyGH5FTktOvKHu/1ZpyRPu')*/?></h5>
+-->
